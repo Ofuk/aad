8080791c86542833575d91e39e05e44995258f59
Ofuk <162102386+Ofuk@users.noreply.github.com>
2024-03-16 16:30:45 +0700

    Add files via upload

---
+++ website/data.js
@@ -0,0 +1,14 @@
+const nama = "adli ulhaq";
+let umur = 19;
+console.log(`Nama saya adalah ${nama} dan Umur saya ${umur} tahun`);
+
+function biodata() {
+    if (umur == 19) {
+        console.log('anda belum menikah')
+
+    }else {
+        console.log('anda tua')
+
+    }
+}
+biodata();
